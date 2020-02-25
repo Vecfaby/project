28b216d760d6acb9dbae0f2706b83de242f1d84a
Vecfaby <61444432+Vecfaby@users.noreply.github.com>
2020-02-25 13:23:55 +0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,31 @@ namespace HubProg
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "admin" && textBox2.Text == "55555")
+
+            {
+                menu m = new menu();
+                m.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+
+                MessageBox.Show("Неправильный логин или пароль");
+            }
+        }
     }
 }
+
